26d1ea765cefdbf2bba225c1f8c792fb35da8f51
Christian Sejer <christian_sejer@hotmail.com>
2020-11-17 15:39:13 +0100

    Added customer use registration

--- CustomerClient/Startup.cs
+++ CustomerClient/Startup.cs
@@ -30,6 +30,7 @@ namespace CustomerClient
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<ICloudService, CloudService>();
+            services.AddSingleton<IUserService, CloudUserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
