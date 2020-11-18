5d86fd35675d5284d46b1abf15f1d7a9a07c53a1
Kilgarra <baratanett.b@gmail.com>
2020-11-18 10:49:25 +0100

    Search products filter

---
+++ CustomerClient/Data/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using CustomerClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerClient.Data
+{
+    public interface IUserService
+    {
+        User ValidateUser(string userName, string password);
+    }
+}
---
+++ CustomerClient/Data/InMemoryUserService.cs
@@ -0,0 +1,46 @@
+﻿using CustomerClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerClient.Data
+{
+    public class InMemoryUserService:IUserService
+    {
+        private List<User> users;
+
+        public InMemoryUserService()
+        {
+            users = new[] {
+                new User {
+                    Password = "123456",
+                    SecurityLevel = 1,
+                    UserName = "Kevin"
+                },
+                new User {
+                    Password = "123456",
+                    SecurityLevel = 1,
+                    UserName = "Anett"
+                }
+            }.ToList();
+        }
+
+
+        public User ValidateUser(string userName, string password)
+        {
+            User first = users.FirstOrDefault(user => user.UserName.Equals(userName));
+            if (first == null)
+            {
+                throw new Exception("User not found");
+            }
+
+            if (!first.Password.Equals(password))
+            {
+                throw new Exception("Incorrect password");
+            }
+
+            return first;
+        }
+    }
+}
